46aa86f2e6185aa56caeffcb32e190dc3e32eea6
Liquid Angel <paolo.pasia@gmail.com>
2021-09-07 19:59:02 +0800

    added new files

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+const title = 'Buzzer Beater';
+
+let data = {
+  users: new Set(),
+  buzzes: new Set()
+}
+
+const getData = () => ({
+  users: [...data.users],
+  buzzes: [...data.buzzes].map(b => {
+    const [name, id] = b.split('-');
+    return {name, id};
+  })
 });
 
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('index', {title}));
+app.get('/host', (req, res) => res.render('host', Object.assign({ title }, getData())));
+
 io.on('connection', (socket) => {
-  console.log('a user has connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
